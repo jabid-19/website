a5be9cd8630f08cebc164a5f704809b73b7f2bba
Jabid Hasan <jabid19191@gmail.com>
2019-10-15 11:36:55 +0600

    Added skills

--- js/script.js
+++ js/script.js
@@ -34,3 +34,20 @@ const scene= new ScrollMagic.Scene({
 .setTween(tween)
 // .addIndicators()
 .addTo(controller);
+
+//skill
+
+var skillsDiv = jQuery('.about');
+
+jQuery(window).on('scroll', function() {
+  var winT = jQuery(window).scrollTop(),
+    winH = jQuery(window).height(),
+    skillsT = skillsDiv.offset().top;
+  if (winT + winH > skillsT) {
+    jQuery('.skillbar').each(function() {
+      jQuery(this).find('.skillbar-bar').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 6000);
+    });
+  }
+});
