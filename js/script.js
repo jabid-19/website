4e92a0313643f9f4581fd8580967d6ac80b94fc1
jabid-19 <jabid19191@gmail.com>
2021-02-13 09:07:49 +0600

    bugs fix

--- js/script.js
+++ js/script.js
@@ -30,37 +30,12 @@ const scene = new ScrollMagic.Scene({
   triggerHook: 0,
 })
   .setTween(tween)
-  // .addIndicators()
   .addTo(controller);
-
-//skill
-
-var skillsDiv = jQuery(".about");
-
-jQuery(window).on("scroll", function () {
-  var winT = jQuery(window).scrollTop(),
-    winH = jQuery(window).height(),
-    skillsT = skillsDiv.offset().top;
-  if (winT + winH > skillsT) {
-    jQuery(".skillbar").each(function () {
-      jQuery(this)
-        .find(".skillbar-bar")
-        .animate(
-          {
-            width: jQuery(this).attr("data-percent"),
-          },
-          6000
-        );
-    });
-  }
-});
 
 // top Button
 
-//Get the button
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
@@ -74,10 +49,10 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // function openModal() {
 //   document.getElementById("myModal").style.display = "block";
@@ -119,49 +94,3 @@ function scrollFunction() {
 //   dots[slideIndex - 1].className += " active";
 //   captionText.innerHTML = dots[slideIndex - 1].alt;
 // }
-
-window.addEventListener("DOMContentLoaded", function () {
-  // get the form elements defined in your form HTML above
-
-  var form = document.getElementById("conForm");
-  // var button = document.getElementById("my-form-button");
-  // var status = document.getElementById("status");
-
-  // Success and Error functions for after the form is submitted
-
-  function success() {
-    form.reset();
-    // status.classList.add("success");
-    // status.innerHTML = "Thanks!";
-  }
-
-  function error() {
-    // status.classList.add("error");
-    // status.innerHTML = "Oops! There was a problem.";
-  }
-
-  // handle the form submission event
-
-  form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data);
-  });
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
