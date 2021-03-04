50a872da5c681f6f77d3f4bef4749d192e34f193
jabid-19 <jabid19191@gmail.com>
2021-03-04 08:56:25 +0600

    bugs fix

--- js/script.js
+++ js/script.js
@@ -32,27 +32,6 @@ const scene = new ScrollMagic.Scene({
   .setTween(tween)
   .addTo(controller);
 
-// top Button
-var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // function openModal() {
 //   document.getElementById("myModal").style.display = "block";
 // }
@@ -121,9 +100,12 @@ $(document).ready(function () {
 
   //top button
   var mybutton = document.getElementById("myBtn");
+  var progress = document.getElementById("progress");
+  var w = window.innerWidth;
 
   window.onscroll = function () {
     scrollFunction();
+    myFunction();
   };
 
   function scrollFunction() {
@@ -132,8 +114,14 @@ $(document).ready(function () {
       document.documentElement.scrollTop > 20
     ) {
       mybutton.style.display = "block";
+      if (w > 800) {
+        progress.style.display = "block";
+      } else {
+        progress.style.display = "none";
+      }
     } else {
       mybutton.style.display = "none";
+      progress.style.display = "none";
     }
   }
 
@@ -141,6 +129,16 @@ $(document).ready(function () {
   function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+  }
+
+  function myFunction() {
+    var winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    var height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
   }
 
   //mail
